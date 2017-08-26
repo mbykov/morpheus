4d1f94183f30ea61d68e2fcb3d9042a36853b568
Michael Bykov <m.bykov@gmail.com>
2017-08-26 22:45:35 +0300

    0.4.6

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ function createWindow () {
 
     mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
+        // in an array if your app supports multi windows, this is the time this is the time
         // when you should delete the corresponding element.
         timerId = null
         mainWindow = null
