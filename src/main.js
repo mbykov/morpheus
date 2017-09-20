47835eed5b1dfb37b43cbd5d32ff43b093695867
Michael Bykov <m.bykov@gmail.com>
2017-09-20 16:47:57 +0300

    0.4.84

--- src/main.js
+++ src/main.js
@@ -140,6 +140,7 @@ app.on('ready', () => {
     tray.setContextMenu(contextMenu)
 })
 
+//
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
